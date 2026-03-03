d77bb9af50aeceb6681239c4806d5cf435c6a772
Priyanshi Sai <priyanshisainicky@gmail.com>
2026-03-03 23:54:37 +0530

    done

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,7 +1,11 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import {prisma} from "@/lib/prisma";
+import {PrismaAdapter} from "@auth/prisma-adapter";
 
 export const authConfig = {
+    secret: process.env.AUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
     providers: [
         // We leave this empty or minimal here; we'll define the
         // authorize logic in the main auth.ts file to keep Prisma out of here.
