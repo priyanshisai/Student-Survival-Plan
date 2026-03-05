38505e11ebea0e80bef68dfcb3e31b0f0e2a9def
Priyanshi Sai <priyanshisainicky@gmail.com>
2026-03-05 19:51:15 +0530

    Stop tracking filename.ext

--- src/supabase-client.ts
+++
@@ -1,5 +0,0 @@
-import {createClient} from "@supabase/supabase-js";
-export const supabse = createClient(
-    "https://icuzhwmpazkulwamxdpk.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljdXpod21wYXprdWx3YW14ZHBrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjUzMjQ1OCwiZXhwIjoyMDg4MTA4NDU4fQ.wNyT0tlTBdxWJSoDlK_xk7bp0UYOec47VM27BruKut0"
-    );
